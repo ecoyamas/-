c46102e1e3624fbbb092217766d29a7914102076
ecoyamas <hayato53pu@gmail.com>
2018-04-05 19:40:56 +0900

    Javascriptエラー

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ function disp(){
 	window.alert('準備中です。');
 
 }
+
+
+var id = "1st";
+　   document.getElementById(id).textContent=3*3+1*1;
+var id = "2nd";
+　   document.getElementById(id).textContent=3*2+1*2;
+var id = "3rd";
+　   document.getElementById(id).textContent=3*2+1*1;
+var id = "4th";
+　   document.getElementById(id).textContent=3*1+1*2;
+var id = "5th";
+　   document.getElementById(id).textContent=3*1+1*1;
