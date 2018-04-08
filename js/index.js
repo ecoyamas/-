d7f65eef6a6571addf246451b52799b974c0b48d
fumimi <info@fumimi.jp>
2018-04-08 09:33:33 +0900

    勝ち点の計算を実装

--- js/index.js
+++ js/index.js
@@ -4,9 +4,16 @@ function disp(){
 
 }
 
+var win1 = document.getElementById('win1').textContent;
+var draw1 = document.getElementById('draw1').textContent;
+var lose1 = document.getElementById('lose1').textContent;
 
-var id ='1st'
-　   document.getElementById(id).textContent=3*3+1*1;
+var result;
+result = win1 * 3 + draw1 * 1 + lose1 * 0;
+
+
+var id = '1st';
+document.getElementById(id).textContent = result;
 var id = "2nd";
 　   document.getElementById(id).textContent=3*2+1*2;
 var id = "3rd";
